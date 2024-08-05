2ab0b35923ba37a5e56ee50029ffa0f97722ed64
normanbareo <normanpatrickbareo@gmail.com>
2024-08-06 00:25:01 +0800

    Add files via upload

---
+++ HCI 2/login.js
@@ -0,0 +1,6 @@
+
+    document.getElementById('dropdownMenuButton').addEventListener('click', function() 
+    {
+        var dropdownContent = document.getElementById('dropdownContent');
+        dropdownContent.classList.toggle('show');
+    });
